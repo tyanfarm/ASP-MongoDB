5dc2f7ecaf6caeb5f4e0fad52d837b57db2f7035
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-03-19 14:22:20 +0700

    Update DepartmentController

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -19,12 +19,54 @@ namespace ASP_MongoDB.Controllers
 
         [HttpGet]
         public JsonResult Get() {
-            // khi sử dụng MongoClient thì không có xác minh IP
+            // Khởi tạo kết nối
             MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DefaultConnection"));
 
             var dbList = dbClient.GetDatabase("testdb").GetCollection<Department>("Department").AsQueryable();
 
             return new JsonResult(dbList);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department) {
+            // Khởi tạo kết nối
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DefaultConnection"));
+
+            int lastDepartmentID = dbClient.GetDatabase("testdb").GetCollection<Department>("Department").AsQueryable().Count();
+
+            department.DepartmentID = lastDepartmentID + 1;
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").InsertOne(department);
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department department) {
+            // Khởi tạo kết nối
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DefaultConnection"));
+
+            // Tìm DepartmentID trong DB theo ID được đưa vào
+            var filter = Builders<Department>.Filter.Eq("DepartmentID", department.DepartmentID);
+
+            // Update DepartmentName
+            var update = Builders<Department>.Update.Set("DepartmentName", department.DepartmentName);
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").UpdateOne(filter, update);
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id) {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DefaultConnection"));
+            
+            // Tìm DepartmentID trong DB theo ID được đưa vào
+            var filter = Builders<Department>.Filter.Eq("DepartmentID", id);
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").DeleteOne(filter);
+
+            return new JsonResult("Deleted Successfully");
         }
     }
 
